7652c866760b6c1630c303fa54c317887eb651ef
duyanhitbe <duyanh.it.work@gmail.com>
2022-04-07 00:21:39 +0700

    deploy

--- src/Configs/Typeorm.ts
+++ src/Configs/Typeorm.ts
@@ -4,13 +4,13 @@ import { createConnection } from 'typeorm';
 
 export default async function connectTypeorm() {
     await createConnection({
-        type: 'mysql',
+        type: !__prod__ ? 'mysql' : 'postgres',
         username: process.env.TYPEORM_USERNAME,
         password: process.env.TYPEORM_PASSWORD,
         database: process.env.TYPEORM_DATABASE,
         url: __prod__ ? process.env.TYPEORM_URL : undefined,
         synchronize: !__prod__,
-        // logging: !__prod__,
-        entities
+        logging: !__prod__,
+        entities,
     });
 }
